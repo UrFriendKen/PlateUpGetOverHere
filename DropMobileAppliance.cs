b0f0c0897b9f897b987d6b2a85288a7f2f493129
UrFriendKen <51278666+UrFriendKen@users.noreply.github.com>
2023-05-23 22:01:11 +0800

    init

---
+++ DropMobileAppliance.cs
@@ -0,0 +1,45 @@
+﻿using Kitchen;
+using KitchenMods;
+using Unity.Entities;
+using UnityEngine;
+
+namespace KitchenGetOverHere
+{
+    public class DropMobileAppliance : ItemInteractionSystem, IModSystem
+    {
+        protected override InteractionType RequiredType => InteractionType.Grab;
+
+        private Entity Mobile;
+
+        EntityContext ctx;
+
+        protected override bool IsPossible(ref InteractionData data)
+        {
+            ctx = data.Context;
+            if (!ctx.Require(data.Interactor, out CItemHolder holder) || holder.HeldItem == default || !ctx.Has<CMobileAppliance>(holder.HeldItem))
+                return false;
+            Mobile = holder.HeldItem;
+            if (!Bounds.Contains(data.Attempt.Location.Rounded()))
+                return false;
+            return true;
+        }
+
+        protected override void Perform(ref InteractionData data)
+        {
+            Entity occupant = GetOccupant(data.Attempt.Location);
+            if (occupant != default && !ctx.Has<CAllowMobilePathing>(occupant))
+                return;
+
+            ctx.Set(Mobile, new CPosition(data.Attempt.Location.Rounded()));
+            ctx.Remove<CHeldAppliance>(Mobile);
+            ctx.Remove<CHeldBy>(Mobile);
+            ctx.Add<CRemoveView>(Mobile);
+            ctx.Set(Mobile, new CRequiresView
+            {
+                Type = ViewType.Appliance
+            });
+            ctx.Set(data.Interactor, default(CItemHolder));
+            
+        }
+    }
+}
---
+++ PickUpMobileAppliance.cs
@@ -0,0 +1,34 @@
+﻿using Kitchen;
+using KitchenMods;
+
+namespace KitchenGetOverHere
+{
+    public class KitchenGetOverHere : ItemInteractionSystem, IModSystem
+    {
+        protected override InteractionType RequiredType => InteractionType.Grab;
+
+        private EntityContext ctx;
+
+        protected override bool IsPossible(ref InteractionData data)
+        {
+            ctx = data.Context;
+            if (!ctx.Has<CMobileAppliance>(data.Target) || !ctx.Has<CPosition>(data.Target))
+                return false;
+            if (!ctx.Require(data.Interactor, out CItemHolder holder) || holder.HeldItem != default)
+                return false;
+            return true;
+        }
+
+        protected override void Perform(ref InteractionData data)
+        {
+            //ctx.Set(data.Target, default(CPosition));
+            ctx.Set(data.Target, default(CHeldAppliance));
+            ctx.Add<CRemoveView>(data.Target);
+            ctx.Set(data.Target, new CRequiresView
+            {
+                Type = ViewType.HeldAppliance
+            });
+            ctx.UpdateHolder(data.Target, data.Interactor);
+        }
+    }
+}
